e4d1502f750eba1bbb4d3fa377f22e4d633eaacb
Célestin Piaton <celestin.piatonpro@gmail.com>
2025-09-03 15:01:13 +0200

    nvll version bdd .sql

--- sql/BDD_NuaJeu.db.sql
+++ sql/BDD_NuaJeu.db.sql
@@ -1,35 +1,37 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "User" (
-	"id"	INTEGER NOT NULL,
-	"name"	TEXT,
-	PRIMARY KEY("id" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS Carte (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    rarity VARCHAR(50),
+    category VARCHAR(50),
+    size FLOAT
 );
-CREATE TABLE IF NOT EXISTS "user collection" (
-	"id"	INTEGER NOT NULL,
-	"card_id"	INTEGER NOT NULL,
-	"amount"	INTEGER,
-	"user_id"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("user_id") REFERENCES "User"("id"),
-	FOREIGN KEY("card_id") REFERENCES "Carte"("id")
+
+CREATE TABLE IF NOT EXISTS User (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
 );
-CREATE TABLE IF NOT EXISTS "Carte" (
-	"id"	INTEGER NOT NULL,
-	"name"	TEXT,
-	"rarity"	TEXT,
-	"category"	TEXT,
-	"size"	REAL,
-	PRIMARY KEY("id" AUTOINCREMENT)
+
+CREATE TABLE IF NOT EXISTS user_collection (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    card_id INT NOT NULL,
+    amount INT,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES User(id),
+    FOREIGN KEY(card_id) REFERENCES Carte(id)
 );
-INSERT INTO "User" VALUES (1,'user1');
-INSERT INTO "user collection" VALUES (1,1,5,1);
-INSERT INTO "user collection" VALUES (2,2,0,1);
-INSERT INTO "user collection" VALUES (3,3,1,1);
-INSERT INTO "user collection" VALUES (4,4,3,1);
-INSERT INTO "user collection" VALUES (5,5,1,1);
-INSERT INTO "Carte" VALUES (1,'nuage1','common','humeur',9.0);
-INSERT INTO "Carte" VALUES (2,'nuage2','common','troll',2300.0);
-INSERT INTO "Carte" VALUES (3,'nuage3','rare','pixel','0,2');
-INSERT INTO "Carte" VALUES (4,'nuage4','rare','humeur',11000.0);
-INSERT INTO "Carte" VALUES (5,'nuage5','secret','troll',730.0);
-COMMIT;
+
+INSERT INTO User (id, name) VALUES (1, 'user1');
+
+INSERT INTO Carte (id, name, rarity, category, size) VALUES
+(1, 'nuage1','common','humeur',9.0),
+(2, 'nuage2','common','troll',2300.0),
+(3, 'nuage3','rare','pixel',0.2),
+(4, 'nuage4','rare','humeur',11000.0),
+(5, 'nuage5','secret','troll',730.0);
+
+INSERT INTO user_collection (id, card_id, amount, user_id) VALUES
+(1,1,5,1),
+(2,2,0,1),
+(3,3,1,1),
+(4,4,3,1),
+(5,5,1,1);
