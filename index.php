612d5f5031f102991a891f6f13d2f50977cbf1f4
Inès AOUFI <inesaoufii@gmail.com>
2025-09-04 10:33:32 +0200

    reset index

--- index.php
+++ index.php
@@ -7,31 +7,7 @@
     include 'includes/header.php';
 
     require 'includes/db.php';
-
-    try {
-        // Requête pour récupérer toutes les cartes
-        $stmt = $pdo->query("SELECT * FROM carte");
-
-        // Vérifier qu'il y  a des résultats
-        $cartes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        die("Erreur lors de la récupération des cartes : " . $e->getMessage());
-    }
-
 ?>
-
-
-    <ul style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
-        <?php foreach ($cartes as $carte): ?>
-            <li>
-                <?= htmlspecialchars($carte['name']) ?>
-                (Rareté : <?= htmlspecialchars($carte['rarity']) ?>,
-                Catégorie : <?= htmlspecialchars($carte['category']) ?>,
-                Taille : <?= htmlspecialchars($carte['size']) ?>)
-            </li>
-        <?php endforeach; ?>
-    </ul>
-
 
 
 <?php
